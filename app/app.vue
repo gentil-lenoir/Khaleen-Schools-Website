336a5b4d0b82aa206d0d87448b0b74e606fddf0f
Gentil le noir <gentillenoir075@gmail.com>
2025-11-06 17:06:13 +0200

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -4,8 +4,7 @@
   </div>
 </template>
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1907809566957486"
-     crossorigin="anonymous"></script>
+
 <script setup>
 import { useHead } from '#app'
 
