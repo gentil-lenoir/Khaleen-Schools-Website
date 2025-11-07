eb4fbedb57de2b8efe717b67d58cd894c67d8b05
Gentil le noir <gentillenoir075@gmail.com>
2025-11-07 09:16:34 +0200

    Adding Adsterra ads

--- app/components/AdsterraSimple.vue
+++ app/components/AdsterraSimple.vue
@@ -1,23 +1,34 @@
-<!-- components/AdsterraSimple.vue -->
+<!-- Modifie ton AdsterraDirect.vue pour MULTIPLES emplacements -->
 <template>
   <div>
-    <ClientOnly>
-      <!-- Native Banner -->
-      <div :id="`container-${nativeKey}`" class="adsterra-native"></div>
+    <!-- Native Banner TOP -->
+    <div class="adsterra-top" style="margin: 2rem 0; text-align: center;">
       <script 
         async 
         data-cfasync="false" 
-        :src="`//pl28003514.effectivegatecpm.com/${nativeKey}/invoke.js`"
+        src="//pl28003514.effectivegatecpm.com/659cc23355304b9d302d2283f845aa9b/invoke.js"
       ></script>
+      <div id="container-659cc23355304b9d302d2283f845aa9b"></div>
+    </div>
 
-      <!-- Social Bar -->
+    <!-- Native Banner MIDDLE (nouvelle instance) -->
+    <div class="adsterra-middle" style="margin: 3rem 0; text-align: center;">
       <script 
-        type='text/javascript' 
-        :src="`//pl28003490.effectivegatecpm.com/22/cd/2c/${socialKey}.js`"
+        async 
+        data-cfasync="false" 
+        src="//pl28003514.effectivegatecpm.com/659cc23355304b9d302d2283f845aa9b/invoke.js"
       ></script>
-    </ClientOnly>
+      <div id="container-middle-659cc23355304b9d302d2283f845aa9b"></div>
+    </div>
+
+    <!-- Social Bar -->
+    <script 
+      type='text/javascript' 
+      src='//pl28003490.effectivegatecpm.com/22/cd/2c/22cd2ccb8a05c1515a242d2a03acc0a7.js'
+    ></script>
   </div>
 </template>
+
 
 <script setup>
 const nativeKey = '659cc23355304b9d302d2283f845aa9b'
